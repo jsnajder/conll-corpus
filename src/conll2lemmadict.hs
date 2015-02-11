992107cffc2a3a88f1f06317ef066ee91d3326d8
Jan Snajder <jan.snajder@fer.hr>
2015-02-11 15:38:13 +0900

    rev

--- src/conll2lemmadict.hs
+++ src/conll2lemmadict.hs
@@ -3,13 +3,12 @@
 -- 
 -------------------------------------------------------------------------------
 
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, BangPatterns #-}
 
 import ConllReader
 import Control.Applicative
 import Control.Monad
 import Data.Char (isLower)
-import qualified Data.Counts as C
 import Data.Either
 import Data.List
 import Data.Map (Map)
@@ -24,45 +23,50 @@ import qualified Data.Text.IO as T
 import System.Console.ParseArgs
 import System.Environment
 import System.IO
+import Debug.Trace
 
 readWordList :: FilePath -> IO (Set Text)
 readWordList f = 
   S.fromList . map (head . T.words) . T.lines <$> T.readFile f
 
-mapping :: Set Text -> Text -> [(Text,Text)]
+mapping :: Set Text -> String -> [(String,String)]
 mapping ws s = do
-  t <- rights . map (parseLine . T.unpack) $ T.lines s
+  t <- rights . map parseLine $ lines s
   guard $ if S.null ws then True 
     else T.pack (form t) `S.member` ws || 
          T.pack (form t ++ posSep ++ cpostag t) `S.member` ws
   l <- lemma t
   let lp = l ++ posSep ++ cpostag t
       wp = form t ++ posSep ++ cpostag t
-  return (T.pack wp, T.pack lp)
+  return (wp, lp)
 
-mkDict :: [(Text,Text)] -> Map Text Text
-mkDict xs = fmap (fst . maximumBy (comparing snd) . C.counts) d
+mkDict :: [(String,String)] -> Map Text Text
+mkDict xs = fmap (fst . maximumBy (comparing snd) . M.toList) d
   where d = foldl' (\d (w,l) -> ins w l d) M.empty xs
-        ins w l d = M.insertWith (C.union) w (C.fromList [l]) d
+        ins w l d = let w' = T.pack w
+                        l' = T.pack l
+                    in case M.lookup w' d of
+                      Nothing -> M.insert w' (M.singleton l' 1) d
+                      Just c  -> M.adjust (M.insertWith (+) l' 1) w' d
 
-mrMap :: Set Text -> Text -> Text
-mrMap ws = T.unlines . map (\(w,l) -> T.concat [w,"\t",l]) . mapping ws
+mrMap :: Set Text -> String -> String
+mrMap ws = unlines . map (\(w,l) -> w ++ "\t" ++ l) . mapping ws
 
-mrReduce :: Text -> Text
+mrReduce :: String -> String
 mrReduce = 
-  T.unlines . map (\(w,l) -> T.concat [w,"\t",l]) . M.toList . 
-  mkDict . map parse . T.lines
-  where parse s = case T.splitOn "\t" s of
+  unlines . map (\(w,l) -> T.unpack w ++ "\t" ++ T.unpack l) . M.toList . 
+  mkDict . map parse . lines
+  where parse s = case words s of
                     (w:l:_) -> (w,l)
                     _       -> error "no parse"
 
-readInput :: Maybe FilePath -> IO Text
+readInput :: Maybe FilePath -> IO String
 readInput f = do
   h <- case f of 
     Nothing -> return stdin
     Just f  -> openFile f ReadMode
   hSetEncoding h utf8
-  T.hGetContents h
+  hGetContents h
 
 arg = 
   [ Arg 0 (Just 'm') (Just "map") Nothing 
@@ -83,12 +87,12 @@ main = do
           Just f  -> readWordList f
           Nothing -> return S.empty
   if gotArg args 0 then
-    T.interact $ mrMap ws
+    interact $ mrMap ws
   else if gotArg args 1 then
-    T.interact $ mrReduce
+    interact $ mrReduce
   else if gotArg args 3 then do
     d <- mkDict . mapping ws <$> readInput (getArg args 3)
-    T.putStr . T.unlines . map (\(w,l) -> T.concat [w,"\t",l]) $ M.toList d
+    putStr . unlines . map (\(w,l) -> T.unpack w ++ "\t" ++ T.unpack l) $ M.toList d
   else usageError args "Missing input file."
   hFlush stdout
 
